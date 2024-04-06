f356d6dd697f0aa02c13ad3d60e5f623900acc61
rakkate <rakkateichou@gmail.com>
2024-04-07 07:32:02 +0800

    -.github

---
+++ lua/chadrc.lua
@@ -0,0 +1,21 @@
+-- This file  needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+	theme = "gruvbox",
+  theme_toggle = {"gruvbox", "gruvbox_light"};
+
+  nvdash = {
+    load_on_startup = true
+  }
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+return M
