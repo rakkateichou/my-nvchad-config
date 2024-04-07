44fa663ab6a160b0b30cc9fb6d4068a81ce3dc6e
rakkate <rakkateichou@gmail.com>
2024-04-07 18:51:10 +0800

    mac mappings

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,23 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.opt.autochdir = true
+
+local function open_nvim_tree(data)
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
