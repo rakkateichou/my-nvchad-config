280b055a41bf018abac857ba25c286f42c406459
rakkate <rakkateichou@gmail.com>
2024-05-27 13:21:18 +0800

    highlight

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -47,6 +47,10 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
       ensure_installed = {
         "lua",
         "vimdoc",
